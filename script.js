a08863193ec71964915bf91234e6fbd803834c50
elmagny <62995946+elmagny@users.noreply.github.com>
2021-10-28 21:58:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
 		data.forEach(function(itemLi) {
 			let item = document.createElement('li');
 			item.innerText = (itemLi['PLU'] + ' ' + itemLi['Наименование товар']).toLowerCase();
-			//console.log(data);
+			
+			//document.body.append(item);
 			$('.elastic').append(item);
 		})
 		
@@ -17,22 +18,28 @@ document.querySelector('#elastic').oninput = function() {
 	let val = this.value.trim().toLowerCase();
 	let elastcItems = document.querySelectorAll('.elastic li');
 	if (val != '') {
-		elastcItems.forEach(function(elem) {
-			if (elem.innerText.search(val) == -1) {
-				elem.classList.add('hide');
-				elem.innerHTML = elem.innerText.toLowerCase();
-			} else {
-				elem.classList.remove('hide');
-				let str = elem.innerText;
-				elem.innerHTML = innerMark(str, elem.innerText.search(val), val.length);
-			}
-		});	
+		setTimeout(() => {
+			elastcItems.forEach(function(elem) {
+				if (elem.innerText.search(val) == -1) {
+					elem.classList.add('hide');
+					elem.innerHTML = elem.innerText.toLowerCase();
+				} else {
+					elem.classList.remove('hide');
+					let str = elem.innerText;
+					elem.innerHTML = innerMark(str, elem.innerText.search(val), val.length);
+				}
+			});
+		}, 1500);
+			
 	} else {
-		elastcItems.forEach(function(elem) {
-			elem.classList.remove('hide');
-			elem.innerHTML = elem.innerText.toLowerCase();
-
-		});
+		setTimeout(() => {
+			elastcItems.forEach(function(elem) {
+				elem.classList.remove('hide');
+				elem.innerHTML = elem.innerText.toLowerCase();
+	
+			});
+		}, 1500);
+		
 	}
 }
 
